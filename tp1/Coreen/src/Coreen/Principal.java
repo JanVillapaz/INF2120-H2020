11323d4cb79e04e0c2f6e0b0a161edef5176e5c4
Jan Villapaz <46763901+JanVillapaz@users.noreply.github.com>
2020-05-11 14:32:05 -0400

    Corean tp1

---
+++ tp1/Coreen/src/Coreen/Principal.java
@@ -0,0 +1,10 @@
+package Coreen;
+import javax.swing.SwingUtilities;
+
+public class Principal {
+    public static void main( String arg[] ) {
+
+        SwingUtilities.invokeLater( () -> new Fenetre() );
+
+    }
+}
